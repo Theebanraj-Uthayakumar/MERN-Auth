2e094eec8a0604f366884e45b3dac4ea9607c405
Theeban <theebanrajuthayakumar10@gmail.com>
2023-12-25 18:34:57 +0530

    user signup implementation

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
+
+// Login User
+const loginUser = async (req, res) => {
+  res.json({ mssg: " login user" });
+};
+
+// Signup User
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { loginUser, signupUser };
